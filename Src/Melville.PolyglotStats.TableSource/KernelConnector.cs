581c24068f38f187c6c55f0f78add852ee717f2e
John Melville <JohnMelville@gmail.com>
2023-09-30 11:47:22 -0400

    Can register the read-tables kernel

---
+++ Src/Melville.PolyglotStats.TableSource/KernelConnector.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using Microsoft.DotNet.Interactive;
+using Microsoft.DotNet.Interactive.CSharp;
+
+namespace Melville.PolyglotStats.TableSource;
+
+public class KernelConnector
+{
+    public static void AddKernelToRoot()
+    {
+        if (KernelInvocationContext.Current?.HandlingKernel.RootKernel is CompositeKernel ck)
+        {
+            AddKernelTo(ck);
+        }
+    }
+
+    private static void AddKernelTo(CompositeKernel parentKernel)
+    {
+        var rootKernel = parentKernel.ChildKernels.OfType<CSharpKernel>().First();
+        parentKernel.Add(new TableKernel(rootKernel));
+    }
+}
---
+++ Src/Melville.PolyglotStats.TableSource/TableKernel.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Html;
+using Microsoft.DotNet.Interactive;
+using Microsoft.DotNet.Interactive.Commands;
+using Microsoft.DotNet.Interactive.CSharp;
+
+namespace Melville.PolyglotStats.TableSource;
+
+public class TableKernel: Kernel, IKernelCommandHandler<SubmitCode>
+{
+
+    private readonly CSharpKernel innerKernel;
+    public TableKernel(CSharpKernel innerKernel) : base("read-tables")
+    {
+        this.innerKernel = innerKernel;
+    }
+
+    public  Task HandleAsync(SubmitCode command, KernelInvocationContext context)
+    {   context.Display(new HtmlString($"""
+                                 <details>
+                                     <summary>Success</summary>
+                                     <p>{command.Code}</p>
+                                 </details>
+                                 """));
+        context.Display(context);
+        return ((IKernelCommandHandler<SubmitCode>)innerKernel)
+            .HandleAsync(new SubmitCode("""
+                                        var title = "Hello World";
+                                        """), context);
+    }
+}
